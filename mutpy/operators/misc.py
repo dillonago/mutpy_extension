d862be5aaab5f99c55f42ec715d8c3c4935a83fc
Jai Parera <67336232+NFJ1618@users.noreply.github.com>
2024-11-19 20:57:36 -0800

    Fixing test_operator.py

--- mutpy/operators/misc.py
+++ mutpy/operators/misc.py
@@ -26,28 +26,33 @@ class ConstantReplacement(MutationOperator):
     FIRST_CONST_STRING = 'mutpy'
     SECOND_CONST_STRING = 'python'
 
-    def mutate_Num(self, node):
-        return ast.Num(n=node.n + 1)
+    def mutate_Constant(self, node):
+        if isinstance(node.value, (int, float)):  # Handle numbers
+            return ast.Constant(value=node.value + 1)
 
-    def mutate_Str(self, node):
-        if utils.is_docstring(node):
-            raise MutationResign()
+        elif isinstance(node.value, str):  # Handle strings
+            if utils.is_docstring(node):
+                raise MutationResign()
 
-        if node.s != self.FIRST_CONST_STRING:
-            return ast.Str(s=self.FIRST_CONST_STRING)
-        else:
-            return ast.Str(s=self.SECOND_CONST_STRING)
+            if node.value != self.FIRST_CONST_STRING:
+                return ast.Constant(value=self.FIRST_CONST_STRING)
+            else:
+                return ast.Constant(value=self.SECOND_CONST_STRING)
 
-    def mutate_Str_empty(self, node):
-        if not node.s or utils.is_docstring(node):
-            raise MutationResign()
+        elif node.value is not None:  # Handle NameConstant (True, False, None)
+            return ast.Constant(value=None)
 
-        return ast.Str(s='')
+        raise MutationResign()  # Skip mutation if the constant isn't relevant
+
+    def mutate_Constant_empty(self, node):
+        if isinstance(node.value, str) and node.value and not utils.is_docstring(node):
+            return ast.Constant(value='')  # Replace non-empty strings with empty strings
+
+        raise MutationResign()  # Skip for non-string nodes or docstrings
 
     @classmethod
     def name(cls):
         return 'CRP'
-
 
 class SliceIndexRemove(MutationOperator):
     def mutate_Slice_remove_lower(self, node):
